c1982a4c4843f4ad46aecfb11ff3edfa0806b5c0
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-11 13:49:44 +0300

    add swagger

--- typing_trainer/settings.py
+++ typing_trainer/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'trainer',
     'users',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- typing_trainer/urls.py
+++ typing_trainer/urls.py
@@ -4,12 +4,30 @@ from trainer import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Typing Trainer API",
+        default_version='v1',
+        description="API для аутентификации, работы с пользователями и статистики тренировок",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.typing_test, name='typing_test'), 
     path('learn/', views.learn, name='learn'),
     path('custom/', views.custom, name='custom'),
     path('users/', include('users.urls')),
+    path('swagger/', schema_view.as_view(), name='swagger-docs'),
 ]
 
 if settings.DEBUG:
